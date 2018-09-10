469b4380bc3b8ae04d2574fbaef52a24a2f2fe5f
Bogdan Markov <bogdick@yandex.ru>
2018-09-10 04:05:03 +0300

    Improvements.
    Moved constants.
    Added exceptions handling and page loading timeout.
    Optimized imports.
    Added comments.

--- src/parser.py
+++ src/parser.py
@@ -1,36 +1,60 @@
-from selenium import webdriver
-from os import path
 import pathlib
 
-page_path = pathlib.Path(
-    path.abspath('../res/10000_-----Ticketmaster Delivery.htm')).as_uri()
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException, \
+    NoSuchElementException
 
 
-# todo: exceptions handling
-# todo: move constants
+# program constants
+PAGE_PATH = '../res/10000_-----Ticketmaster Delivery.htm'
+PAGE_URI = pathlib.Path(PAGE_PATH).absolute().as_uri()
+FIELD_NAMES = ('Section', 'Row', 'Seats', 'Subtotal', 'Time left')
+PAGE_LOAD_TIMEOUT = 3 * 60  # in seconds; this equals to 3 minutes
+
+# web page constants
+CONTAINER_CLASS_NAME = 'container-neutralZone'
+ELEMENTS_CLASS_NAME = 'shipValueRight'
+SUBTOTAL_CSS_SELECTOR = '.shipValueRight.messageText'
+CLOCK_MINUTES_ID = 'countdown_clock_minutes'
+CLOCK_SECONDS_ID = 'countdown_clock_seconds'
+
 
 def parse():
+    # open browser and get page
     driver = webdriver.Firefox()
-    driver.get(page_path) # todo: add timeout
+    driver.set_page_load_timeout(PAGE_LOAD_TIMEOUT)
+    try:
+        driver.get(PAGE_URI)
+    except WebDriverException as e:
+        print(e)
+        driver.close()
+        return
 
+    # dictionary with extracted data
     data = {}
-
-    container = driver.find_element_by_class_name('container-neutralZone')
-    elems = [
-        value.text for value in container.find_elements_by_class_name(
-            'shipValueRight')[:3]
-    ]
-    data['Section'] = elems[0]
-    data['Row'] = elems[1]
-    data['Seats'] = elems[2]
-    data['Subtotal'] = driver.find_element_by_css_selector('.shipValueRight.messageText').text
-    data['Time left'] = '{}:{}'.format(
-        driver.find_element_by_id('countdown_clock_minutes').text,
-        driver.find_element_by_id('countdown_clock_seconds').text,
-    )
-    driver.close()
-
-    print(data)
+    try:
+        # get section, row and seats
+        container = driver.find_element_by_class_name(CONTAINER_CLASS_NAME)
+        data.update(
+            (name, value.text) for name, value in zip(
+                FIELD_NAMES[:3],
+                container.find_elements_by_class_name(ELEMENTS_CLASS_NAME),
+            )
+        )
+        # get subtotal
+        data[FIELD_NAMES[3]] = \
+            driver.find_element_by_css_selector(SUBTOTAL_CSS_SELECTOR).text
+        # get time left
+        data[FIELD_NAMES[4]] = '{}:{}'.format(
+            driver.find_element_by_id(CLOCK_MINUTES_ID).text,
+            driver.find_element_by_id(CLOCK_SECONDS_ID).text,
+        )
+    except NoSuchElementException as e:
+        print(e)
+    else:
+        print(data)
+    finally:
+        driver.close()
 
 
 if __name__ == '__main__':
